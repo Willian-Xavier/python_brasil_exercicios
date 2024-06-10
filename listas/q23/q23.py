9e29dc5dee876d17a28ee0e22b0b9a66d39f85f3
Willian Xavier <106575614+Willian-Xavier@users.noreply.github.com>
2024-06-09 22:54:47 -0300

    Commit

--- listas/q23/q23.py
+++ listas/q23/q23.py
@@ -1,15 +1,20 @@
-# A ACME Inc., uma empresa de 500 funcionários, está tendo problemas de espaço em disco no seu servidor de arquivos.
-# Para tentar resolver este problema, o Administrador de Rede precisa saber qual o espaço ocupado pelos usuários, e
-# identificar os usuários com maior espaço ocupado. Através de um programa, baixado da Internet, ele conseguiu gerar
-# o seguinte arquivo, chamado "usuarios.txt":
+# A ACME Inc., uma empresa de 500 funcionários, está tendo problemas de espaço
+# em disco no seu servidor de arquivos. Para tentar resolver este problema,
+# o Administrador de Rede precisa saber qual o espaço ocupado pelos usuários,
+# e identificar os usuários com maior espaço ocupado. Através de um programa,
+# baixado da Internet, ele conseguiu gerar o seguinte arquivo, chamado
+# "usuarios.txt":
 #
-# Neste arquivo, o nome do usuário possui 15 caracteres. A partir deste arquivo, você deve criar um programa que gere
-# um relatório, chamado "relatório.txt", no seguinte formato:
+# Neste arquivo, o nome do usuário possui 15 caracteres. A partir deste
+# arquivo, você deve criar um programa que gere um relatório, chamado
+# "relatório.txt", no seguinte formato:
 #
-# O arquivo de entrada deve ser lido uma única vez, e os dados armazenados em memória, caso sejam necessários, de forma
-# a agilizar a execução do programa. A conversão da espaço ocupado em disco, de bytes para megabytes deverá ser feita
-# através de uma função separada, que será chamada pelo programa principal. O cálculo do percentual de uso também
-# deverá ser feito através de uma função, que será chamada pelo programa principal.
+# O arquivo de entrada deve ser lido uma única vez, e os dados armazenados em
+# memória, caso sejam necessários, de forma a agilizar a execução do programa.
+# A conversão da espaço ocupado em disco, de bytes para megabytes deverá ser
+# feita através de uma função separada, que será chamada pelo programa
+# principal. O cálculo do percentual de uso também deverá ser feito através
+# de uma função, que será chamada pelo programa principal.
 import math
 
 usuarios = []
@@ -42,8 +47,9 @@ with open('relatorio.txt', mode='w') as dado:
 
     for itera in range(len(usuarios)):
         tamanho_uso = converter_mebibyte(uso_disco[itera])
-        dado.write(f'{itera + 1:<6}{usuarios[itera]:<15} {tamanho_uso:>12.2f} MB '
-                   f'{calcula_porcentagem_uso(tamanho_uso, uso_total_disco):>14.2f}%\n')
+        dado.write(
+            f'{itera + 1:<6}{usuarios[itera]:<15} {tamanho_uso:>12.2f} MB '
+            f'{calcula_porcentagem_uso(tamanho_uso, uso_total_disco):>14.2f}%\n')
 
     dado.write('-' * 58)
     dado.write(f'\nEspaço total ocupado: {uso_total_disco:.2f} MB\n')
